30a52b284d1608b324a30761ac12a6a663fb39c4
Kainom <eleuteriokaina@gmail.com>
2024-06-08 00:25:39 -0300

    teste

--- src/viewcalc/src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        // Calculator conta = new Calculator();
-        Calc cacl = new Calc();
-
-        
-    }
-}
